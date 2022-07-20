bbab0b6c8cc5f46a63c34aafdd168bfd412d6e51
Sumit Chouhan <sumit.chouhan@technoidentity.com>
2022-07-20 17:12:51 +0530

    Second Version

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 function getData() {
   var data = [
     {
-      id: "First Blog",
+      id: 0,
+      title: "First Blog",
+      author: "Sachin Tendulkar",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
     {
-      id: "Second Blog",
+      id: 1,
+      title: "Second Blog",
+      author: "Virat Kohli",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
     {
-      id: "Third Blog",
+      id: 2,
+      title: "Third Blog",
+      author: "Rishabh Pant",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
     {
-      id: "Fourth Blog",
+      id: 3,
+      title: "Fourth Blog",
+      author: "Jaspreeet Bumrah",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
     {
-      id: "Fifth Blog",
+      id: 4,
+      title: "Fifth Blog",
+      author: "M.S. Dhoni",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
     {
-      id: "Sixth Blog",
+      id: 5,
+      title: "Sixth Blog",
+      author: "Rohit Sharma",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor morbi non arcu risus quis. Nibh tortor id aliquet lectus proin nibh nisl. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Ullamcorper dignissim cras tincidunt lobortis feugiat. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Amet luctus venenatis lectus magna. Leo duis ut diam quam nulla porttitor. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Sapien et ligula ullamcorper malesuada proin. Euismod nisi porta lorem mollis aliquam ut porttitor. Eget velit aliquet sagittis id consectetur purus ut. Tellus molestie nunc non blandit massa enim. Quis vel eros donec ac. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Est velit egestas dui id.Vel elit scelerisque mauris pellentesque pulvinar. Etiam non quam lacus suspendisse faucibus. Feugiat pretium nibh ipsum consequat nisl vel. Pharetra sit amet aliquam id diam maecenas ultricies mi. Massa ultricies mi quis hendrerit dolor magna eget. Turpis egestas maecenas pharetra convallis. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sollicitudin aliquam ultrices sagittis orci. Non consectetur a erat nam at. Fermentum leo vel orci porta. Sapien et ligula ullamcorper malesuada proin libero. Nam at lectus urna duis convallis convallis tellus id interdum. Malesuada fames ac turpis egestas maecenas. Fermentum odio eu feugiat pretium nibh. Netus et malesuada fames ac turpis egestas sed tempus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Ac auctor augue mauris augue neque. Enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor dignissim convallis aenean et. Lobortis feugiat vivamus at augue.Integer enim neque volutpat ac tincidunt vitae semper quis. Aliquet nibh praesent tristique magna sit. Enim nulla aliquet porttitor lacus luctus. In ornare quam viverra orci sagittis. Tristique magna sit amet purus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Diam quis enim lobortis scelerisque. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Porta nibh venenatis cras sed felis eget. Leo duis ut diam quam nulla porttitor. Mauris a diam maecenas sed enim ut. Condimentum lacinia quis vel eros donec ac. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Lectus magna fringilla urna porttitor rhoncus dolor purus.",
     },
   ];
@@ -34,41 +46,76 @@ function getData() {
 
   outerMost.appendChild(innerHeading);
 
-  for (let i = 0; i < data.length; i++) {
+  data.map((d) => {
     var outer = document.createElement("DIV");
     outer.className = "innerChild1";
     var inner1 = document.createElement("H1");
     inner1.className = "innerText";
-    inner1.innerHTML = data[i].id;
+    inner1.innerHTML = d.title;
     var inner2 = document.createElement("BUTTON");
     inner2.className = "collapsible";
     inner2.innerHTML = "Expand";
+    inner2.id = d.id;
     var inner3 = document.createElement("DIV");
     inner3.className = "content";
+    var authorHeading = document.createElement("H1");
+    authorHeading.innerText = d.author;
     var para = document.createElement("P");
     para.id = "demo";
-    const node = document.createTextNode(data[i].text);
+    const node = document.createTextNode(d.text);
     para.appendChild(node);
-    inner3.append(para);
+    inner3.append(authorHeading, para);
     outer.append(inner1, inner2, inner3);
     outerMost.appendChild(outer);
     document.body.append(outerMost);
-  }
+  });
+
+  // for (let i = 0; i < data.length; i++) {
+  //   var outer = document.createElement("DIV");
+  //   outer.className = "innerChild1";
+  //   var inner1 = document.createElement("H1");
+  //   inner1.className = "innerText";
+  //   inner1.innerHTML = data[i].id;
+  //   var inner2 = document.createElement("BUTTON");
+  //   inner2.className = "collapsible";
+  //   inner2.innerHTML = "Expand";
+  //   var inner3 = document.createElement("DIV");
+  //   inner3.className = "content";
+  //   var para = document.createElement("P");
+  //   para.id = "demo";
+  //   const node = document.createTextNode(data[i].text);
+  //   para.appendChild(node);
+  //   inner3.append(para);
+  //   outer.append(inner1, inner2, inner3);
+  //   outerMost.appendChild(outer);
+  //   document.body.append(outerMost);
+  // }
 }
 
 getData();
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      changeTextOnClose(i);
     } else {
       content.style.display = "block";
+      changeTextOnOpen(i);
     }
   });
+}
+
+function changeTextOnOpen(i) {
+  var btn = document.getElementById(i);
+  btn.innerHTML = "Collapse";
+}
+
+function changeTextOnClose(i) {
+  var btn = document.getElementById(i);
+  btn.innerHTML = "Expand";
 }
